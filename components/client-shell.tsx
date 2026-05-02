3519ccdfc2f3a9bdd31e3bb778485910f6a9f05b
mohamed <medelfankari.com>
2026-05-02 16:53:38 +0100

    Rearranged the navbar

--- components/client-shell.tsx
+++ components/client-shell.tsx
@@ -70,10 +70,10 @@ export function ClientShell({ children }: { children: React.ReactNode }) {
 
           <div className="nav-links" id="site-nav-links">
             <a href="/#home">Home</a>
-            <a href="/#about">About</a>
-            <a href="/#education">Education</a>
-            <a href="/#skills">Skills</a>
             <a href="/#projects">Projects</a>
+            <a href="/#experience">Experience</a>
+            <a href="/#skills">Skills</a>
+            <a href="/#about">About</a>
             <a href="/#contact">Contact</a>
           </div>
         </div>
